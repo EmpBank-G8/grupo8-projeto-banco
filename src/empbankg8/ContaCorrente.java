26f5bdc97bcc2e5407feba49444c68874143c674
jamtibaes <jamtibaes@yahoo.com.br>
2022-07-25 20:38:08 -0300

    Adicionando Classe Conta Corrente e Conta Poupança

---
+++ src/empbankg8/ContaCorrente.java
@@ -0,0 +1,8 @@
+package empbankg8;
+
+public class ContaCorrente extends ContaPrincipal{
+
+    public ContaCorrente(String cpf, String nome, String senha) {
+        super(cpf, nome, senha);
+    }
+}
---
+++ src/empbankg8/ContaPoupanca.java
@@ -0,0 +1,10 @@
+package empbankg8;
+
+public class ContaPoupanca extends ContaPrincipal{
+
+    public ContaPoupanca(String cpf, String nome, String senha) {
+        super(cpf, nome, senha);
+    }
+
+
+}
