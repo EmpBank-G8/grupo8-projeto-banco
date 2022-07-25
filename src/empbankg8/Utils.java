2eb5accc0f901862078d0d1a3bb77a32fd3e48ac
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-25 00:12:05 -0300

    Criação de classe para métodos gerais

---
+++ src/empbankg8/Utils.java
@@ -0,0 +1,23 @@
+package empbankg8;
+
+public class Utils {
+	public static void imprimeMenuPrincipal() {
+		System.out.println("*************************************");
+		System.out.println("*    Bem-vindo(a) ao Emp Bank-G8    *");
+		System.out.println("*************************************\n");
+		System.out.println("1 - Abrir Conta");
+		System.out.println("2 - Acessar Conta");
+		System.out.println("0 - Sair");
+		System.out.print("Selecione a opção desejada: ");
+	}
+	public static void imprimirMenuOpcoesContas() {
+		System.out.println("\n1 - Conta Poupança");
+		System.out.println("2 - Conta Corrente");
+		System.out.println("3 - Conta Especial");
+		System.out.println("4 - Conta Empresa");
+		System.out.println("5 - Conta Estudantil");
+		System.out.println("6 - Retorne ao menu principal");
+		System.out.println("0 - Sair");
+		System.out.print("Escolha a opção desejada: ");
+	}
+}
