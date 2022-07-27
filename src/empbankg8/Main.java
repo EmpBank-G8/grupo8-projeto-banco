b9b059c9e90ee65620743136397e69d038f5fe15
crissoa <csoares.cris@gmail.com>
2022-07-26 22:51:46 -0300

    Adiciona menu de informação de conta

--- src/empbankg8/Main.java
+++ src/empbankg8/Main.java
@@ -2,29 +2,35 @@ package empbankg8;
 
 import java.util.Scanner;
 
+import empbankg8.entities.ContaEstudantil;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//Colocar Slogan
+		// Colocar Slogan
 		Scanner entrada = new Scanner(System.in);
 		Utils.imprimeMenuPrincipal();
 		int opcao = entrada.nextInt();
-		
-		while (opcao != 0) {
-			
-			if(opcao == 1) {
-				Utils.imprimirMenuOpcoesContas();
-				opcao = entrada.nextInt();
-			}else if (opcao == 2) {
-				System.out.println("Acessar Conta");
-			}else if(opcao == 0) {
-				System.out.println("Sair");
-			}
-			else {
-				System.out.println("\nOpção inválida...");
-				Utils.imprimeMenuPrincipal();
-				opcao = entrada.nextInt();
-			}
+
+		// while (opcao != 0) {
+
+		if (opcao == 1) {
+			Utils.imprimirMenuOpcoesContas();
+			opcao = entrada.nextInt();
+		} else if (opcao == 2) {
+			System.out.println("Acessar Conta");
+
+			ContaPrincipal contaEstudantil = new ContaEstudantil("123.045.456-78", "Cristiane Soares", "1234");
+			Utils.imprimirDadosDaConta(contaEstudantil);
+			Utils.imprimirMenuMovimentacao();
+
+		} else if (opcao == 0) {
+			System.out.println("Sair");
+		} else {
+			System.out.println("\nOpção inválida...");
+			Utils.imprimeMenuPrincipal();
+			opcao = entrada.nextInt();
 		}
+//		} 
 	}
 }
--- src/empbankg8/Utils.java
+++ src/empbankg8/Utils.java
@@ -2,15 +2,15 @@ package empbankg8;
 
 public class Utils {
 	public static void imprimeMenuPrincipal() {
-		System.out.println("*************************************");
-		System.out.println("*    Bem-vindo(a) ao Emp Bank-G8    *");
-		System.out.println("*************************************\n");
+		imprimeCabecalho();
 		System.out.println("1 - Abrir Conta");
 		System.out.println("2 - Acessar Conta");
 		System.out.println("0 - Sair");
 		System.out.print("Selecione a opção desejada: ");
 	}
+
 	public static void imprimirMenuOpcoesContas() {
+		imprimeCabecalho();
 		System.out.println("\n1 - Conta Poupança");
 		System.out.println("2 - Conta Corrente");
 		System.out.println("3 - Conta Especial");
@@ -20,4 +20,27 @@ public class Utils {
 		System.out.println("0 - Sair");
 		System.out.print("Escolha a opção desejada: ");
 	}
-}
+
+	public static void imprimirDadosDaConta(ContaPrincipal conta) {
+		imprimeCabecalho();
+		System.out.println("\nCONTA " +  conta.getTipoConta());
+		System.out.println("\nNome: " +   conta.getNome());
+		System.out.println("CPF: " +   conta.getCpf());
+		System.out.println("\nSaldo Atual: R$ " + conta.getSaldo() + 
+				" - " + conta.getInformacaoEspecifica());
+		
+	}
+	
+	public static void imprimirMenuMovimentacao() {
+		System.out.println("\nEscolha o tipo de movimentação: ");
+		System.out.println("C - Crédito");
+		System.out.println("D - Débito");
+		
+	}
+	private static void imprimeCabecalho() {
+		System.out.println("*************************************");
+		System.out.println("*    Bem-vindo(a) ao Emp Bank-G8    *");
+		System.out.println("*************************************\n");
+	}
+
+} 
