f9fcfb846dc16fa1bb048f88e1730bae20ebebff
jamtibaes <jamtibaes@yahoo.com.br>
2022-07-26 22:22:25 -0300

    Modificações basicas

--- src/empbankg8/ContaCorrente.java
+++ src/empbankg8/ContaCorrente.java
@@ -2,7 +2,19 @@ package empbankg8;
 
 public class ContaCorrente extends ContaPrincipal{
 
+    private int contadorTalao = 3;
+
     public ContaCorrente(String cpf, String nome, String senha) {
         super(cpf, nome, senha);
     }
+
+    public void pedirTalao(){
+        contadorTalao--;
+    }
+
+
+
+
+
+
 }
--- src/empbankg8/ContaPoupanca.java
+++ src/empbankg8/ContaPoupanca.java
@@ -2,9 +2,20 @@ package empbankg8;
 
 public class ContaPoupanca extends ContaPrincipal{
 
+    private int diaAniversarioPoupanca;
+
     public ContaPoupanca(String cpf, String nome, String senha) {
         super(cpf, nome, senha);
     }
+
+    public double correcao(int dataInformada, double saldo){
+        if (dataInformada>=diaAniversarioPoupanca){
+            return (saldo*0.05) + saldo;
+        }
+        return saldo;
+    }
+
+
 
 
 }
