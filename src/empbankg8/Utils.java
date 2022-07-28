0cefcbaeed5b785ed57609ac892cbaf6efe9e93e
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-27 22:14:14 -0300

    Atualização utils

--- src/empbankg8/Utils.java
+++ src/empbankg8/Utils.java
@@ -1,16 +1,24 @@
 package empbankg8;
 
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+
 public class Utils {
-	public static void imprimeMenuPrincipal() {
+	public static void imprimeBemVindo() {
 		System.out.println("*************************************");
-		System.out.println("*    Bem-vindo(a) ao Emp Bank-G8    *");
-		System.out.println("*************************************\n");
-		System.out.println("1 - Abrir Conta");
+		System.out.println("*      Bem-vindo(a) ao G8-Bank      *");
+		System.out.println("*************************************");
+	}
+	public static void imprimeMenuPrincipal() {
+		System.out.println("\n* MENU PRINCIPAL *");
+		System.out.println("\n1 - Abrir Conta");
 		System.out.println("2 - Acessar Conta");
 		System.out.println("0 - Sair");
 		System.out.print("Selecione a opção desejada: ");
 	}
 	public static void imprimirMenuOpcoesContas() {
+		
 		System.out.println("\n1 - Conta Poupança");
 		System.out.println("2 - Conta Corrente");
 		System.out.println("3 - Conta Especial");
@@ -19,5 +27,28 @@ public class Utils {
 		System.out.println("6 - Retorne ao menu principal");
 		System.out.println("0 - Sair");
 		System.out.print("Escolha a opção desejada: ");
+	}
+	public static void imprimirOpcoesContaEspecial() {
+		
+		System.out.println("Qual operação você deseja realizar?\n ");
+		System.out.println("1 - Credito");
+		System.out.println("2 - Débito");
+		System.out.println("3 - Usar limite");
+		System.out.println("4 - Consultar saldo");
+		System.out.println("5 - Limite disponível");
+		System.out.println("0 - Sair");
+		System.out.print("Escolha a opção desejada: ");
+	}
+	public static int login(int numConta, String codSenha, ArrayList<ContaEspecial> array) {
+		for(int i = 0; i < array.size(); i++) {
+			if(numConta ==(array.get(i).getNumero()) && codSenha.equals(array.get(i).getSenha())) {
+				return i;
+			}
+		}
+		return -1;
+	
+	}
+	public static void imprimirSaidaBanco() {
+		System.out.println("Obrigado por usar o G8-Bank!");
 	}
 }
