05566eadb823a60f35ce88f41a14c7e8c1192bfd
jamtibaes <jamtibaes@yahoo.com.br>
2022-07-31 18:28:14 -0300

    Atualizando main com ContaCorrente e ContaPoupanca

--- src/empbankg8/entities/ContaPoupanca.java
+++ src/empbankg8/entities/ContaPoupanca.java
@@ -1,36 +1,55 @@
 package empbankg8.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContaPoupanca extends ContaPrincipal {
 
+
     private int diaAniversarioPoupanca;
+    private double rendimentos = 0;
 
     public ContaPoupanca(String cpf, String nome, String senha) {
         super(cpf, nome, senha, "POUPANÇA");
     }
 
+    public void credito(double valor, int dataSistema) {
+        credito(valor);
+        this.diaAniversarioPoupanca = dataSistema;
+    }
+
     public void correcao(int dataInformada){
-        if (dataInformada > diaAniversarioPoupanca){
-            this.credito(this.getSaldo() * (0.5 / 100));
+        if (dataInformada >= diaAniversarioPoupanca){
+            this.rendimentos = getSaldo() * 0.005;
         }
     }
 
-    public int getDiaAniversarioPoupanca() {
-        return diaAniversarioPoupanca;
+    public void debitar(double valor, int dataDebito) {
+        if (this.getSaldo() < valor){
+            System.out.println("Não foi possível realizar o debito");
+            return;
+        }
+        this.credito(this.rendimentos);
+        this.rendimentos = 0;
+        this.setNumMovimentacao(getnumMovimentacao() - 1);
+        this.debito(valor);
     }
 
-    public void setDiaAniversarioPoupanca(int diaAniversarioPoupanca) {
-        this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+
+    public double getRendimentos() {
+        return rendimentos;
     }
 
-@Override
+    @Override
 	public String getInformacaoEspecifica() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Rendimentos no mês: R$" + this.getRendimentos();
 	}
 
     @Override
     public String toString() {
         return "\nNúmero: " + this.getNumero() + " \n" + "Nome: " + this.getNome() + " \n" + "Cpf: " + this.getCpf() + " \n" + "Saldo: " + this.getSaldo();
     }
-  
+
+
 }
--- src/empbankg8/entities/ContaPrincipal.java
+++ src/empbankg8/entities/ContaPrincipal.java
@@ -72,6 +72,10 @@ public abstract class ContaPrincipal {
 		return numMovimentacao;
 	}
 
+	protected void setNumMovimentacao(int numMovimentacao) {
+		this.numMovimentacao = numMovimentacao;
+	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
@@ -85,6 +89,8 @@ public abstract class ContaPrincipal {
 		}
 		
 	}
+
+
 	public void credito(double valor) {
 		if (this.numMovimentacao < 10) {
 			this.saldo = this.saldo + valor;
@@ -103,5 +109,7 @@ public abstract class ContaPrincipal {
 	}
 
 	public abstract String getInformacaoEspecifica();
+
+
 }
 
