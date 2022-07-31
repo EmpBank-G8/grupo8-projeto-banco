9bc7eb09389200aa11b20e4de3cdaea923982820
crissoa <csoares.cris@gmail.com>
2022-07-31 18:54:53 -0300

    corrige conta empresa

--- src/empbankg8/entities/ContaEmpresa.java
+++ src/empbankg8/entities/ContaEmpresa.java
@@ -15,11 +15,11 @@ public class ContaEmpresa extends ContaPrincipal {
 	}
 	
 	public void usarEmpresa(double valor) {
-		if (valor <= limiteEstudantil) {
+		if (valor <= emprestimoEmpresa) {
 			credito(valor);
 			this.emprestimoEmpresa = this.emprestimoEmpresa - valor;
 		}else {
-			System.out.println("Operação não autorizada. " + "Valor disponível: " + this.limiteEstudantil);
+			System.out.println("Operação não autorizada. " + "Valor disponível: " + this.emprestimoEmpresa);
 			
 		}
 	}
